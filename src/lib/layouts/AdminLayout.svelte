f97c4a39d767c20a258d2f3d9dac075b0d9b2137
Rookie Nguyen <nhqthang@proton.me>
2026-08-02 01:14:56 +0700

    chore: rename for brevity

---
+++ src/lib/layouts/AdminLayout.svelte
@@ -0,0 +1,20 @@
+<script>
+    let { children } = $props();
+</script>
+
+<div class="grid h-screen grid-rows-[auto_1fr]">
+   <!-- Header -->
+   <!-- <Header /> -->
+
+   <!-- Body Grid -->
+   <div class="grid grid-cols-[auto_1fr]">
+       <!-- Sidebar -->
+       <!-- <Sidebar /> -->
+
+       <!-- Content -->
+       <main class="">
+           {@render children()}
+       </main>
+
+   </div>
+</div>
---
+++ src/routes/admin/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import AdminLayout from '$lib/layouts/AdminLayout.svelte';
+
+	let { children } = $props();
+</script>
+
+<AdminLayout>
+	{@render children()}
+</AdminLayout>
