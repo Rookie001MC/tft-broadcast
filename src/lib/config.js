0a18eb2b6574617226cb3229c1b6e2a837b448ad
Rookie Nguyen <nhqthang@proton.me>
2026-08-03 17:35:02 +0700

    feat: add basic global metadata

---
+++ src/lib/config.js
@@ -0,0 +1,6 @@
+const siteConfig = {
+	siteName: "HCMUSEC TFT Winner GFX",
+	siteDescription: "Broadcast Graphic System for Teamfight Tactics Tournaments, by VNUHCM - University of Science Esports Club"
+}
+
+export default siteConfig;
