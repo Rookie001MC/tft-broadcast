ab8e00523394090d639fadb658bc05e356a22f7e
Rookie Nguyen <nhqthang@proton.me>
2026-08-05 12:28:29 +0700

    fix: also duplicate the dynamic meta title

--- src/routes/(admin)/+layout.svelte
+++ src/routes/(admin)/+layout.svelte
@@ -1,7 +1,19 @@
 <script>
 	import "./layout.css";
-
+	import siteConfig from "$lib/config.js";
+	import { setPageMetaContext } from "$lib/context/pageMetaContext";
 	const { children } = $props();
+	const pageMeta = $state({ title: undefined, description: undefined });
+	setPageMetaContext(pageMeta);
+	const headerTitle = $derived(pageMeta.title);
+	const description = $derived(pageMeta.description);
+
 </script>
+
+
+<svelte:head>
+	<title>{headerTitle != null ? `${headerTitle} ${siteConfig.separator} ` : ''}{siteConfig.siteName}</title>
+	<meta name="description" content={description ? description : siteConfig.siteDescription} />
+</svelte:head>
 
 {@render children()}
